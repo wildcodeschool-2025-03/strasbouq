cb8be2008ca98d8dcdee12185925ac65c8a07f30
darasom69 <daha69@hotmail.fr>
2025-04-17 17:50:38 +0200

    add cgv

--- client/src/pages/Cgv.tsx
+++ client/src/pages/Cgv.tsx
@@ -1,5 +1,67 @@
+import Footer from "../components/footer/Footer";
+import Header from "../components/header/Header";
+
+
 function Cgv() {
-  return <></>;
+  return (
+    <>
+      <Header />
+
+      <h1 className="text-center text-secondary m-6 ">
+        Conditions générales de vente{" "}
+      </h1>
+    <p>
+        1. Objet et champ d'application Les CGV visent à définir les relations
+        contractuelles et modalités de vente entre Les Jardin Alsacien et tout
+        Client souhaitant procéder à un achat sur le site internet Les Jardin
+        Alsacien, et en appelant le Centre relation clients Les Jardin Alsacien.
+        Le service de transmission florale Les permet au Client d'effectuer une
+        commande afin de faire livrer à distance, par un fleuriste membre du
+        réseau INTERFLORA (le « Fleuriste Les Jardin Alsacien ») ou par
+        colis-fleurs via les transporteurs Les Jardin Alsacien, un produit
+        floral ou une plante à un destinataire, accompagné ou non d'un message
+        ou d'un autre produit non floral (chocolat, bougie, vin, peluche, etc.)
+        (les « Produits »), à une adresse, une date et un créneau horaire donnés
+        (le « Service Les Jardin Alsacien »). Toute commande d'un Produit
+        proposé via un des Canaux de Vente Les Jardin Alsacien implique
+        l'adhésion du Client aux présentes CGV consultables sur le Site Les
+        Jardin Alsacien.
+        <br />
+        2. Les Jardin Alsacien FRANCE Le réseau Les Jardin Alsacien compte de
+        nombreux fleuristes indépendants affiliés sur tout le territoire
+        français. Ce réseau de fleuristes créateurs permet un maillage de
+        proximité. Dans le cadre de la transmission florale, Les Jardin Alsacien
+        s'engage sur la qualité des fleuristes Membres agréés de son réseau
+        autorisés à faire usage de la marque Les Jardin Alsacien. Les Jardin
+        Alsacien veille au respect, par chacun, des engagements qui suivent et,
+        de manière générale, à la bonne application du contrat en vigueur avec
+        les Fleuristes INTERFLORA, pour un parfait respect des droits du
+        consommateur.
+        <br />
+        3. Caractéristique des Produits - Prix Caractéristique des Produits
+        Seuls les Produits figurant sur le Site Les Jardin Alsacien au jour de
+        sa consultation par le Client sont proposés à la vente. Les Produits
+        sont proposés à la vente dans la limite des stocks disponibles. Ceux-ci
+        sont mis à jour régulièrement, sauf problème technique indépendant Les
+        Jardin Alsacien. A défaut de disponibilité du Produit, le Client est
+        informé dans les meilleurs délais. Le Client déclare avoir eu
+        connaissance des caractéristiques essentielles des Produits qui figurent
+        sur le Site Les Jardin Alsacien dans la description du Produit. Le
+        Client est informé que chaque produit floral est unique et que le
+        Fleuriste Les Jardin Alsacien peut, une fois la commande du Client reçue
+        et selon son stock disponible ou la saisonnalité des fleurs, être amené
+        à substituer une variété de fleurs pour réaliser sa création et offrir
+        au Client le Produit floral le plus fidèle à sa Commande. Cette
+        substitution ne concerne que les fleurs secondaires à savoir celles qui
+        ne sont pas déterminantes dans le choix du bouquet. Ces spécificités
+        induisent que la sensibilité créative du Fleuriste Les Jardin Alsacien
+        ou son savoir-faire, peuvent faire que la création livrée puisse, à son
+        initiative, parfois légèrement différer du visuel.
+      </p>
+
+      <Footer />
+    </>
+  );
 }
 
 export default Cgv;
