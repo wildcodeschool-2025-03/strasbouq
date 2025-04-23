61a79d4506167047a6a7927f90dc8f055b662e58
DURNERIN NATHAN <durnerin.nathan01@gmail.com>
2025-04-23 10:02:03 +0200

    Add page mon compte avec menu accordeon

---
+++ client/src/components/compte/AccordeonItem.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+interface AccordeonItemProps {
+  title: string;
+  children: React.ReactNode;
+}
+
+const AccordeonItem = ({ title, children }: AccordeonItemProps) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  return (
+    <div className=" py-2 ">
+      <button
+        type="button"
+        onClick={() => setIsOpen(!isOpen)}
+        className="flex justify-between items-center w-full text-left font-medium text-lg"
+      >
+        <span className="font-semibold">{title}</span>
+        <span className="text-xl">{isOpen ? "−" : "+"}</span>
+      </button>
+      <div
+        className={`mt-2 overflow-hidden transition-all duration-300 ease-in-out ${
+          isOpen ? "max-h-96 opacity-100" : "max-h-0 opacity-0"
+        }`}
+      >
+        <div className="pt-2 bg-secondary text-sm pl-3 ">{children}</div>
+      </div>
+    </div>
+  );
+};
+
+export default AccordeonItem;
--- client/src/pages/Compte.tsx
+++ client/src/pages/Compte.tsx
@@ -1,5 +1,15 @@
+import Accordeon from "../components/compte/Accordeon";
+
 function Compte() {
-  return <></>;
+  return (
+    <>
+      <section className="bg-primary mt-10">
+        <h2 className="ml-5 pt-5 text-2xl font-bold">Bonjour Nom Prenom</h2>
+        <hr className="ml-5 mt-5 h-px border-t-0 bg-black w-[80%]" />
+        <Accordeon />
+      </section>
+    </>
+  );
 }
 
 export default Compte;
