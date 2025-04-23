8e5016e5e0f563fa74b199fb5e5c0a04313b78ab
Davidlevy13 <levy.david1993@gmail.com>
2025-04-23 17:05:09 +0200

    début création filtres en cours

--- client/src/components/catalogue/Contenu_catalogue.tsx
+++ client/src/components/catalogue/Contenu_catalogue.tsx
@@ -20,20 +20,24 @@ function Contenu({ item }: ContenuProps) {
 
   return (
     <section className="flex-col justify-items-left bg-[#EADED5] rounded-md relative px-5 pt-70 pb-10 w-[300px] mx-auto overflow-visible">
-      <img className="absolute top-[-50px] left-[0px]"
+      <img
+        className="absolute top-[-50px] left-[0px]"
         src={item.image_url}
         alt={item.nom}
       />
       <h3 className="font-bold pb-3">{item.nom}</h3>
       <p className="pb-3">{item.description}</p>
       <aside className="pb-3 font-bold">{item.prix} €</aside>
-     <section className="flex flex-row justify-between">
-      <button type="button" onClick={handleClick}>
-        {!isFavorite ? "🖤" : "❤️"}
-      </button>
-      <button className="bg-[#CE9170] rounded-4xl p-2 pr-4 pl-4 font-bold" type="button">
-        Commander
-      </button>
+      <section className="flex flex-row justify-between">
+        <button type="button" onClick={handleClick}>
+          {!isFavorite ? "🖤" : "❤️"}
+        </button>
+        <button
+          className="bg-[#CE9170] rounded-4xl p-2 pr-4 pl-4 font-bold"
+          type="button"
+        >
+          Commander
+        </button>
       </section>
     </section>
   );
--- client/src/pages/Catalogue.tsx
+++ client/src/pages/Catalogue.tsx
@@ -1,9 +1,34 @@
 import { useEffect, useState } from "react";
 import Contenu from "../components/catalogue/Contenu_catalogue";
 
+interface itemsContenu {
+  id: number;
+  nom: string;
+  description: string;
+  prix: number;
+  image_url: string;
+}
+
 function Catalogue() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState<itemsContenu[]>([]);
   const API_URL = import.meta.env.VITE_API_URL;
+  const [order, setOrder] = useState(false);
+  const [minPrice, setMinPrice] = useState(0);
+  const [maxPrice, setMaxPrice] = useState(100);
+  const [color, setColor] = useState("");
+
+  const flowerTri = () => {
+    const finalTable: itemsContenu[] = [];
+    const tableau: itemsContenu[] = items;
+
+    for (const flower of tableau) {
+      if (flower.prix < maxPrice && flower.prix > minPrice) {
+        finalTable.push(flower);
+      }
+    }
+
+    setItems(finalTable);
+  };
 
   useEffect(() => {
     fetch(API_URL)
@@ -14,7 +39,7 @@ function Catalogue() {
   return (
     <section className="grid md:grid-cols-2 xl:grid-cols-3 gap-20 pt-20 pb-20">
       {items?.map((item) => (
-        <Contenu item={item} key={item} />
+        <Contenu item={item} key={item.id} />
       ))}
     </section>
   );
