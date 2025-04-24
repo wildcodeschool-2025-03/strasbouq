08a03796d72e9fb17f662a0794e70de1faa112fc
ARNAUD DEVOGE <arnoo.54@hotmail.fr>
2025-04-24 15:54:23 +0200

    add fonction panier mon_compte

--- client/src/pages/Panier.tsx
+++ client/src/pages/Panier.tsx
@@ -1,5 +1,55 @@
+import { useEffect, useState } from "react";
+import Contenu from "../components/catalogue/Contenu_catalogue";
+
+interface itemsContenu {
+  id: number;
+  nom: string;
+  description: string;
+  prix: number;
+  image_url: string;
+}
+
+interface Utilisateur {
+  mail: string;
+  panier: itemsContenu[] | null;
+}
+
 function Panier() {
-  return <></>;
+  const [panier, setPanier] = useState<itemsContenu[]>([]);
+
+  useEffect(() => {
+    const listUser = localStorage.getItem("users");
+    if (listUser === null) {
+      alert("Veuillez vous connecter");
+      return;
+    }
+    const users: Utilisateur[] = JSON.parse(listUser);
+
+    const userConnected = sessionStorage.getItem("currentUser");
+    if (userConnected === null) {
+      alert("Veuillez vous connecter");
+      return;
+    }
+    const connected = JSON.parse(userConnected);
+
+    const user = users.find((u) => u.mail === connected.mail);
+
+    if (!user || !user.panier || user.panier.length === 0) {
+      alert("Mon panier est vide");
+    } else {
+      setPanier(user.panier);
+    }
+  }, []);
+
+  return (
+    <>
+      {panier.length === 0 ? (
+        <p>Votre panier est vide.</p>
+      ) : (
+        panier.map((bouquet) => <Contenu item={bouquet} key={bouquet.id} />)
+      )}
+    </>
+  );
 }
 
 export default Panier;
